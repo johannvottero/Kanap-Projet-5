3d26f0b4687c59de3a623685077335158043b933
Johann <johannvottero@gmail.com>
2023-01-02 19:45:00 +0100

    commit mise à jour, ajout requête fetch

--- front/js/script.js
+++ front/js/script.js
@@ -1,12 +1,60 @@
+// requête à l'api pour obtention des données produits
 fetch("http://localhost:3000/api/products")
 .then(function(res) {
-    if (res.ok) {
       return res.json();
     }
-  })
+)
   .then(function(value) {
     console.log(value);
+    let altTxt = value[0].altTxt
+    let description = value[0].description
+    let imageUrl = value[0].imageUrl
+    let name = value[0].name
+    let price = value[0].price
+    let id = value[0]._id
+
+    const titreProduit = document.createElement("h3")
+    titreProduit.text = "canapé exemple"
+    console.log(titreProduit)
+
+    const kanapName = document.getElementsByClassName("productName")
+
   })
-  .catch(function(err) {
-    // Une erreur est survenue
-  });
+
+
+
+
+  console.log(items)
+
+/*
+  parentNode.appendChild(<element>)
+
+  /* elt.setAttribute("type", "password");  
+/*
+function product 
+
+/*
+document.createElement("a")
+let link = document.createElement("a") 
+
+/*
+  for (let i = 0; i < res.length; i++) {
+    console.log(i);
+ }
+
+
+/*
+p.classList.add('productDescription')
+p.innerText = data.description
+console.log('productDescription')
+
+/*
+  const productName = document.getElementsByClassName('productName');
+  console.log('ProductName')
+
+*/
+
+/* 
+const tableauProduit = [value]
+console.log('value')
+*/
